d5f9da7d6ad6f5b6fb4dea50d55ff8284bc50b45
QyperXit <67711154+QyperXit@users.noreply.github.com>
2023-11-22 13:27:25 +0000

    clearing

--- gh-pages/index.13dd4529.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector(".fa-bars");document.querySelector(".hide");let d=document.querySelector(".modal");e.addEventListener("click",e=>{d.classList.toggle("hidden"),d.classList.contains("hidden")}),window.addEventListener("resize",()=>{window.innerWidth<750&&d.classList.add("hidden")});
-//# sourceMappingURL=index.13dd4529.js.map
